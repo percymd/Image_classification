bf6a6a566a25f5324aecc700e8020c2dfa932cb2
Percy Marquez <percymd10@gmail.com>
2021-07-05 14:09:31 -0500

    Prediction and randomize images

--- scripts.js
+++ scripts.js
@@ -9,6 +9,28 @@ async function app(){
 
     var result = await net.classify(imgEl);
     console.log(result);
+    displayImagePrediction();
+}
+
+imgEl.onload = async function(){
+
+    displayImagePrediction();
+}
+
+async function displayImagePrediction(){
+    try{
+        result = await net.classify(imgEl);
+        descEl.innerHTML = JSON.stringify(result);
+    }catch(error){
+
+    }
+}
+
+count = 0
+async function cambiarImagen(){
+    count = count+1;
+    imgEl.src = "https://picsum.photos/200/300?random=" + count;
+
 }
 
 app();
